791d61df9852e8cbb64dfa8dfb945e2cee2cd25a
Caitlyn <craigoda+caitlyn@gmail.com>
2021-08-17 07:45:32 -0700

    adding listIterator

--- src/org/codecakes/Main.java
+++ src/org/codecakes/Main.java
@@ -1,16 +1,46 @@
 package org.codecakes;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
     static ArrayList<Album> albumArrayList = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 	// write your code here
 
 
         loadData();
         Playlist playlist = new Playlist(albumArrayList);
-       printAlbumLibrary();
+      // printAlbumLibrary();
+       playlist.addSong(new Song("Hello Future", "2:45"));
+       playlist.addSong(new Song("Rocket", "2:56"));
+       playlist.addSong(new Song("Magic", "2:40"));
+       playlist.addSong(new Song("Anti-Romantic", "3:36"));
+       playlist.addSong(new Song("Diggity", "3:30"));
+       playlist.printPlaylist();
+       menuOfOptions();
+       boolean quit = false;
+
+        ListIterator<Song> listIterator = playlist.getSongLinkedList().listIterator();
+        System.out.println("Currently playing " + listIterator.next().getTitle());
+       while(!quit){
+           int userInput = scanner.nextInt();
+scanner.nextLine();
+           switch (userInput){
+               case 0:
+                   quit = true;
+                   break;
+               case 1:
+                   System.out.println("Now playing " + listIterator.next().getTitle());
+                   break;
+               case 4:
+                   playlist.printPlaylist();
+                   break;
+           }
+       }
+
     }
 
     public static void loadData(){
@@ -38,8 +68,10 @@ public class Main {
     }
 
     public static void menuOfOptions(){
+
         System.out.println("List of Options:");
         System.out.println("0 - quit");
-
+        System.out.println("1 - skip forward");
+        System.out.println("4 - print playlist");
     }
 }
--- src/org/codecakes/Playlist.java
+++ src/org/codecakes/Playlist.java
@@ -16,7 +16,31 @@ this.albumArrayList = albumArrayList;
     }
 
     public void addSong(Song song){
-        System.out.println("adding song");
-        songLinkedList.add(song);
+        if(checkSong(song.getTitle()) == null){
+            System.out.println("Song does not appear in album");
+        }else{
+            System.out.println("adding song");
+            songLinkedList.add(song);
+        }
+
+    }
+
+    private Song checkSong(String songName){
+        for(int i = 0; i<albumArrayList.size(); i++){
+            Album album = albumArrayList.get(i);
+            for(int j = 0; j<album.getSongArrayList().size(); j++){
+                if(songName.equals(album.getSongArrayList().get(j).getTitle())){
+                    return album.getSongArrayList().get(j);
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public void printPlaylist(){
+        for(int i = 0; i< songLinkedList.size(); i++){
+            System.out.println(songLinkedList.get(i).getTitle() + " at duration " + songLinkedList.get(i).getDuration());
+        }
     }
 }
